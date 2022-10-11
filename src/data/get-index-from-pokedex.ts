2fabf824685bc1974169bb848e79d8687c16365b
Jason Godesky <jefgodesky@gmail.com>
2022-10-11 19:51:12 -0400

    Add JSDoc
    Gotta slow down a bit; getting sloppy, forgot my JSDoc documentation

--- src/data/get-index-from-pokedex.ts
+++ src/data/get-index-from-pokedex.ts
@@ -1,5 +1,15 @@
 import PokeData from './pokedeata'
 
+/**
+ * This is a utility function that returns the index of the `PokeData` in the
+ * array that has the given Pokédex.
+ * @param {PokeData[]} arr - An array of `PokeData` objects to search through.
+ * @param {number} pokedex - The Pokédex number to search for.
+ * @returns {number} - The index of the `PokeData` object with the given
+ *   Pokédex number if it is in the array. If it is not in the array, the
+ *   method returns -1.
+ */
+
 const getIndexFromPokedex = (arr: PokeData[], pokedex: number): number => {
   return arr.findIndex(item => item.pokedex === pokedex)
 }
