0a3d69a6f055bf692222eb6b0c5f67fe1a7e2342
Jason Godesky <jefgodesky@gmail.com>
2022-10-10 20:31:29 -0400

    Bring create-react-app boilerplate up to Standard TS

--- src/components/App/App.test.tsx
+++ src/components/App/App.test.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from 'react'
+import { render, screen } from '@testing-library/react'
+import App from './App'
 
 test('renders learn react link', () => {
-  render(<App />);
-  const h1 = screen.getByText(/what’s your favorite pokémon?/i);
-  expect(h1).toBeInTheDocument();
-});
+  render(<App />)
+  const h1 = screen.getByText(/what’s your favorite pokémon?/i)
+  expect(h1).toBeInTheDocument()
+})
--- src/components/App/App.tsx
+++ src/components/App/App.tsx
@@ -1,9 +1,9 @@
-import React from 'react';
+import React, { ReactElement } from 'react'
 
-function App() {
+function App (): ReactElement {
   return (
     <h1>What’s your favorite Pokémon?</h1>
-  );
+  )
 }
 
-export default App;
+export default App
