a0c0704359a9f8041d07c083816bbf040f24d285
Jason Godesky <jefgodesky@gmail.com>
2022-10-11 20:24:54 -0400

    App & Pokemon components: Pass loadPokemon prop through

--- src/components/Pokemon/Pokemon.tsx
+++ src/components/Pokemon/Pokemon.tsx
@@ -1,15 +1,20 @@
-import React, { ReactElement } from 'react'
+import React, { ReactElement, useRef, MutableRefObject } from 'react'
+import useOnScreen from '../../hooks/use-on-screen'
 import PokeData from '../../data/pokedeata'
 
 interface PokemonProps {
   data: PokeData
+  load: Function
 }
 
-function Pokemon ({ data }: PokemonProps): ReactElement {
-  const { pokedex, name } = data
+function Pokemon ({ data, load }: PokemonProps): ReactElement {
+  const { loaded, pokedex, name } = data
+  const ref = useRef<HTMLElement>() as MutableRefObject<HTMLElement>
+  const isVisible = useOnScreen(ref)
+  if (isVisible && !loaded) load(data)
 
   return (
-    <article>
+    <article ref={ref}>
       <p className='pokedex'>#{pokedex.toString().padStart(3, '0')}</p>
       <h2>{name}</h2>
     </article>
