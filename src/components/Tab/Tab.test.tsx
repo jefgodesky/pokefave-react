07ca4046a50591bdc9173e71c398993cd2fb9803
Jason Godesky <jefgodesky@gmail.com>
2022-10-11 22:16:13 -0400

    Tab: Add onClick prop

--- src/components/Tab/Tab.test.tsx
+++ src/components/Tab/Tab.test.tsx
@@ -1,17 +1,26 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
+import userEvent from '@testing-library/user-event'
 import Tab from './Tab'
 
 describe('Tab', () => {
   it('displays the tab', () => {
-    render(<Tab label='Test' />)
+    render(<Tab label='Test' onClick={() => {}} />)
     const tab = screen.getByText(/test/i)
     expect(tab).toBeInTheDocument()
   })
 
   it('marks an active tab', () => {
-    render(<Tab label='Test' active />)
+    render(<Tab label='Test' active onClick={() => {}} />)
     const tab = screen.getByText(/test/i)
     expect(tab).toHaveClass('active')
+  })
+
+  it('runs the click prop when clicked', () => {
+    const mock = jest.fn()
+    const { container } = render(<Tab label='Test' onClick={mock} />)
+    const btn = container.querySelector('button')
+    userEvent.click(btn as Element)
+    expect(mock).toHaveBeenCalled()
   })
 })
